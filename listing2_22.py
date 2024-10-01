554473c55e6666136a42dbc7cc05fbde9eddbbde
Ainrei <163871964+Ainrei@users.noreply.github.com>
2024-10-01 11:24:08 +0300

    Update listing2_22.py

--- listing2_22.py
+++ listing2_22.py
@@ -1,6 +1,5 @@
 # async нид, потому что task относится к библиотеке async// какой тип переменной возвращает функция - указывать
 import asyncio
-
 async def delay(delay_second: int)-> int:
     print(f'засыпаю на {delay_second} c')
     await asyncio.sleep(delay_second)
@@ -9,7 +8,6 @@ async def delay(delay_second: int)-> int:
 
 def call_later():
     print("Меня вызовут в ближайшем будущем!")
-
 async def main():
     loop = asyncio.get_running_loop()
     loop.call_soon(call_later)
