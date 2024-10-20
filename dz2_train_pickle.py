0c49129e5a2302f04b658e1ea3af5e002cb70042
Ainrei <163871964+Ainrei@users.noreply.github.com>
2024-10-20 14:36:25 +0300

    Update dz2_train_pickle.py

--- dz2_train_pickle.py
+++ dz2_train_pickle.py
@@ -13,7 +13,7 @@ def timethis(func):
         start = time.time()
         result = func(*args, **kwargs)
         end = time.time()
-        print(f"Затрачено {func.__name__} на выполнение {end - start:.8f} сек")
+        print(f"Функция {func.__name__} выполнена за {end - start:.8f} сек")
         return result
     return wrapper
 
