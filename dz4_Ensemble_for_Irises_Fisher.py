185c41d7ddb11a3149bec6a8e0f1df2a67683ba1
Ainrei <163871964+Ainrei@users.noreply.github.com>
2025-01-17 17:11:34 +0300

    Update dz4_Ensemble_for_Irises_Fisher.py

--- dz4_Ensemble_for_Irises_Fisher.py
+++ dz4_Ensemble_for_Irises_Fisher.py
@@ -1,21 +1,41 @@
+# Импортируем модуль asyncio для работы с асинхронным программированием
 import asyncio
+# Импортируем библиотеку numpy для работы с многомерными массивами и матрицами
 import numpy as np
+# Импортируем pandas для работы с данными в табличной форме (DataFrame)
 import pandas as pd
+# Импортируем функцию load_iris из модуля sklearn.datasets для загрузки набора данных "Ирисы Фишера"
 from sklearn.datasets import load_iris
+# Импортируем функцию train_test_split из sklearn.model_selection для разбиения данных на обучающие и тестовые наборы
 from sklearn.model_selection import train_test_split
+# Импортируем модель логистической регрессии из sklearn.linear_model
 from sklearn.linear_model import LogisticRegression
+# Импортируем классификатор случайного леса из sklearn.ensemble
 from sklearn.ensemble import RandomForestClassifier
+# Импортируем метрики для оценки качества модели: accuracy_score и confusion_matrix
 from sklearn.metrics import accuracy_score, confusion_matrix
+
+# Импортируем seaborn для создания красивых визуализаций.
 import seaborn as sns
 import matplotlib.pyplot as plt
+# Импортируем nest_asyncio для поддержки асинхронного программирования в уже существующих циклах событий (например, в Jupyter)
 import nest_asyncio
+
 
 # Для разрешения вложенного выполнения асинхронных функций
 nest_asyncio.apply()
 
 # Ирисы Фишера
+# Загружаем набор данных "Ирисы Фишера" с помощью функции load_iris из sklearn.datasets
 iris = load_iris()
+
+# Преобразуем данные о признаках (features) в DataFrame с помощью pandas
+# Столбцы данных будут соответствовать именам признаков (feature_names)
 df = pd.DataFrame(data=iris['data'], columns=iris['feature_names'])
+
+# Добавляем столбец 'species' в DataFrame, который будет содержать метки классов.
+# Используем pd.Categorical.from_codes для создания категориального столбца на основе кодов в iris['target']
+# и соответствующих названий классов из iris['target_names']
 df['species'] = pd.Categorical.from_codes(iris['target'], iris['target_names'])
 
 # Рассчитаем среднее и стандартное отклонение для каждого вида
@@ -34,30 +54,38 @@ distribution_params.columns = [
     'petal_width_mean', 'petal_width_std'
 ]
 
-# Количество новых записей для генерации
+# Количество новых записей для генерации 1 млн
 num_samples = 10**6
 
 def generate_samples(params, num_samples):
+    # Извлекаем значения среднего для каждого признака, фильтруя столбцы, содержащие 'mean'
     means = params.filter(like='mean').values
+    
+    # Извлекаем значения стандартного отклонения для каждого признака, фильтруя столбцы, содержащие 'std'
     stds = params.filter(like='std').values
+    
+    # Генерируем выборки с помощью нормального распределения
+    # np.random.normal генерирует выборки с указанным средним (means) и стандартным отклонением (stds)
+    # Размер выборки — num_samples, количество признаков — len(means)
     return np.random.normal(loc=means, scale=stds, size=(num_samples, len(means)))
 
-async def async_generate_samples(params, num_samples):
-# асинхронная функция для вызова generate_samples в отдельном фоновом потоке, не блокируя основной поток выполнения
-    return await asyncio.to_thread(generate_samples, params, num_samples)
+async def async_generate_samples(params, num_samples): # асинхронная функция для вызова generate_samples в отдельном фоновом потоке, не блокируя основной поток выполнения
+
+    return await asyncio.to_thread(generate_samples, params, num_samples) # Выполняем generate_samples в фоновом потоке с использованием asyncio.to_thread
 
 async def main():
     tasks = [] # Для хранения асинхронных задач
-    for species, params in distribution_params.iterrows(): # По каждой строке
-        tasks.append(async_generate_samples(params, num_samples))
+    for species, params in distribution_params.iterrows():  # Итерируем по каждой строке в distribution_params (данные о распределении признаков для каждого вида ириса)
+        tasks.append(async_generate_samples(params, num_samples)) # Добавляем асинхронную задачу для генерации выборок
 
     # Ждем завершения всех задач
     results = await asyncio.gather(*tasks)
-
-    # Генерируем данные для каждого вида ириса и объединяем в один DataFrame
+# После завершения задач генерируем DataFrame для каждого вида ириса и объединяем все результаты в один DataFrame
+    
     df_generated = pd.concat(
-        [pd.DataFrame(result, columns=iris['feature_names']).assign(species=species) for result, species in zip(results, distribution_params.index)],
-        ignore_index=True
+        [pd.DataFrame(result, columns=iris['feature_names']).assign(species=species) # Создаем DataFrame для каждой генерации и добавляем столбец вида
+         for result, species in zip(results, distribution_params.index)], # Сопоставляем результаты и индексы (вида ириса)
+        ignore_index=True # Игнорируем индексы для корректного объединения
     )
 
     print(f"Размер сгенерированного датафрейма: {df_generated.shape}")
@@ -72,16 +100,17 @@ async def main():
     # Разделяем на обучающую и тестовую выборки
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
-    # Ансамблевый классификатор
-    log_reg = LogisticRegression(max_iter=1000)
-    log_reg.fit(X_train, y_train)
-    log_reg_predictions = log_reg.predict_proba(X_train)
+    # Ансамблевый классификатор (cоздаем+обучеаем логистическую регрессию)
+    log_reg = LogisticRegression(max_iter=1000) #max число итераций задаем
+    log_reg.fit(X_train, y_train) # Обучаем модель на обучающих данных
+    log_reg_predictions = log_reg.predict_proba(X_train) # Получаем вероятности предсказаний на обучающих данных
+    
+    # Создаем и обучаем классификатор случайного леса
+    rf_classifier = RandomForestClassifier(n_estimators=100, random_state=1) # 100 деревьев в лесу
+    rf_classifier.fit(log_reg_predictions, y_train) # Обучаем модель случайного леса на предсказаниях логистической регрессии
 
-    rf_classifier = RandomForestClassifier(n_estimators=100, random_state=1)
-    rf_classifier.fit(log_reg_predictions, y_train)
-
-    log_reg_test_predictions = log_reg.predict_proba(X_test)
-    rf_predictions = rf_classifier.predict(log_reg_test_predictions)
+    log_reg_test_predictions = log_reg.predict_proba(X_test) # Получаем предсказания для тестовых данных с помощью логистической регрессии
+    rf_predictions = rf_classifier.predict(log_reg_test_predictions) # Получаем предсказания с помощью случайного леса на основе предсказаний логистической регрессии
 
     # Оценка точности ансамбля
     accuracy = accuracy_score(y_test, rf_predictions)
@@ -93,7 +122,7 @@ async def main():
     # Визуализация матрицы ошибок
     plt.figure(figsize=(8, 6))
     sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=['Setosa', 'Versicolor', 'Virginica'],
-                yticklabels=['Setosa', 'Versicolor', 'Virginica'])
+                yticklabels=['Setosa', 'Versicolor', 'Virginica'])  # Аннотируем матрицу с подписями классов
     plt.title('Confusion Matrix')
     plt.xlabel('Predicted')
     plt.ylabel('Actual')
