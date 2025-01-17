ab8bd10ca2cbe6405e25dcfeabed36975dbfb13d
Ainrei <163871964+Ainrei@users.noreply.github.com>
2025-01-17 13:16:53 +0300

    Update f3.py

--- f3.py
+++ f3.py
@@ -1,18 +1,30 @@
+# импортируем time для создания задержек
 import time
+# импортируем multiprocessing для создания и управления дочерними процессами
 import multiprocessing
+# импортируем os, который предоставляет функции для взаимодействия с операционной системой
 import os
+# импортируем datetime для измерения времени выполнения
 import datetime
+
+
+
+# os.getpid() выводит идентификатор текущего исполняемого процесса Python
+# time.sleep() приостанавливает выполнение на кол-во секунд
+# datetime.datetime.now() определает текущее время
+
 def hello_from_process01():
     print(f'Привет от дочернего процесса 01 {os.getpid()}!')
     time.sleep(3)
-    for i in range (100000):
-        2+2
 def hello_from_process02():
     print(f'Привет от дочернего процесса 02 {os.getpid()}!')
     time.sleep(5)
-    for i in range (100000):
-        2+2
 start=datetime.datetime.now()
+
+# проверяем, запущен ли скрипт как основной модуль [if __name__ == '__main__']
+# создаем новый процесс hello_processXX, который будет выполнять функцию hello_from_processXX [hello_process01 = multiprocessing.Process(target=hello_from_process01)]
+# hello_process.start() запускает дочерний процесс
+# hello_process.join() ожидает завершения дочернего процесса перед тем, как продолжить выполнение родительского процесса
 
 if __name__ == '__main__':
     hello_process01 = multiprocessing.Process(target=hello_from_process01)
