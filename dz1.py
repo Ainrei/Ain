56cef5fba2f38779f2ca3fe6aea0c125b11cfd9a
Ainrei <163871964+Ainrei@users.noreply.github.com>
2025-01-17 13:14:34 +0300

    Update dz1.py

--- dz1.py
+++ dz1.py
@@ -1,11 +1,11 @@
 import time
 from functools import wraps
 def timethis(func):
-# Декоратор, который выводит время выполнения.
-    @wraps(func)
-    def wrapper(*args, **kwargs):
+# Декоратор, который выводит время выполнения
+    @wraps(func) # Используем wraps для сохранения метаданных функции 'func'
+    def wrapper(*args, **kwargs): # Определяем внутреннюю функцию 'wrapper', которая обертывает вызов оригинальной функции
         start = time.time()
-        result = func(*args, **kwargs)
+        result = func(*args, **kwargs) # Вызываем оригинальную функцию с аргументами *args и **kwargs и сохраняем результат.
         end = time.time()
         print(func.__name__, end-start)
         return result
